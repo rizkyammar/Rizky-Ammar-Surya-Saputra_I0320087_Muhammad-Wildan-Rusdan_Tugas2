550eb73ae2bc6cd8eb095961febc3f7454f15b1e
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-03-12 16:37:18 +0700

    Add files via upload

---
+++ I0320087_Soal2_Tugas2.py
@@ -0,0 +1,35 @@
+print("Lima Menit Mengenal Saya")
+
+print(">>>>>>>><<<<<<<<")
+
+nama = str("Rizky Ammar surya Saputra")
+ttl = str("Jakarta, 01 Februari 2002")
+umur = int(19)
+nim = str("I0320087")
+ft = str("Fakultas Teknik")
+id = str("Industri")
+at = int("2020")
+bb = float("68.5")
+tb = float("170.05")
+us = int("43")
+ar = str("Jl Klebet V, Jebres, Surakarta")
+
+print("Dibawah ini berisi Biodata saya")
+print("<<<<<<<<>>>>>>>>")
+
+print("1. Nama : ", nama)
+print("2. Tempat, tanggal lahir : ", ttl)
+print("3. Alamat Kost : ", ar)
+print("4. umur : ", umur)
+print("5. Fakultas : ", ft)
+print("6. Jurusan : ", id)
+print("7. NIM : ", nim)
+print("8. Angkatan : ", at)
+print("9. Berat Badan : ", bb)
+print("10. Tinggi Badan : ", tb)
+print("11. Ukuran Sepatu : ", us)
+
+
+print("Terima Kasih Telah Membaca Biodata Saya")
+print(">>>>>>>><<<<<<<<")
+print("<<<<<<<<>>>>>>>>")
