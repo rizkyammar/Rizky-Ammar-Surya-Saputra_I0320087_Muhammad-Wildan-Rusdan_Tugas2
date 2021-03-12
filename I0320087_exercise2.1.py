aabe4c0f827c011ad8608b35f860bdec6cacd303
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-03-12 16:25:01 +0700

    Add files via upload

---
+++ I0320087_exercise2.1.py
@@ -0,0 +1,19 @@
+#tipe data Boolean
+print(True)
+
+#tipe data string
+
+print("Ayo belajar python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print("9a")
+
+#tipe data Complex
+print(5j)
---
+++ I0320087_exercise2.2.py
@@ -0,0 +1,31 @@
+#proses memasukan data ke dalam variabel
+nama = "Rizky Ammar Surya Saputra"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan data dalam variabel dapay diubah
+umur = 19               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "sembilan belas" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Rizky"
+namaBelakang = "Saputra"
+nama = namaDepan + "Ammar Surya" + namaBelakang
+umur = 19
+hobi = "Olahraga"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainnya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+
+print(luas)
